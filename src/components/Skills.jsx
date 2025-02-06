e99ef66a1c6e28e59c75acfb21103a38dbde9a9a
Lspacedev <145987706+Lspacedev@users.noreply.github.com>
2025-02-06 09:50:06 +0200

    updated POE

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -3,58 +3,71 @@ function Skills() {
     {
       icon: "/images/icons/javascript-js.svg",
       text: "JavaScript",
+      level: "Advanced",
     },
     {
       icon: "/images/icons/html-5.svg",
       text: "Html",
+      level: "Advanced",
     },
     {
       icon: "/images/icons/file-type-css.svg",
       text: "CSS",
+      level: "Advanced",
     },
     {
       icon: "/images/icons/typescript-icon.svg",
       text: "TypeScript",
+      level: "Intermediate",
     },
     {
       icon: "/images/icons/React-icon.svg.png",
       text: "React.js",
+      level: "Advanced",
     },
     {
       icon: "/images/icons/React-icon.svg.png",
       text: "React Native",
+      level: "Intermediate",
     },
   ];
   const backend = [
     {
       icon: "/images/icons/icons8-nodejs-48.png",
       text: "Node.js",
+      level: "Advanced",
     },
     {
       icon: "/images/icons/express.png",
       text: "Express.js",
+      level: "Advanced",
     },
     {
       icon: "/images/icons/mongodb.svg",
       text: "MongoDB",
+      level: "Advanced",
     },
     {
       icon: "/images/icons/postgre.png",
       text: "PostgreSQL",
+      level: "Advanced",
     },
     {
       icon: "/images/icons/icons8-prisma-orm-50.png",
       text: "PRISMA",
+      level: "Advanced",
     },
     {
       icon: "/images/icons/firebase.png",
       text: "Firebase",
+      level: "Advanced",
     },
   ];
   const technologies = [
     {
       icon: "/images/icons/icons8-git-60.png",
       text: "Git",
+      level: "Advanced",
     },
   ];
   return (
@@ -68,6 +81,7 @@ function Skills() {
                 <img src={skill.icon} />
               </div>
               <div className="text">{skill.text}</div>
+              <div className="text">{skill.level}</div>
             </div>
           ))}
         </div>
@@ -82,6 +96,7 @@ function Skills() {
                 <img src={skill.icon} />
               </div>
               <div className="text">{skill.text}</div>
+              <div className="text">{skill.level}</div>
             </div>
           ))}
         </div>
@@ -95,6 +110,7 @@ function Skills() {
                 <img src={skill.icon} />
               </div>
               <div className="text">{skill.text}</div>
+              <div className="text">{skill.level}</div>
             </div>
           ))}
         </div>
